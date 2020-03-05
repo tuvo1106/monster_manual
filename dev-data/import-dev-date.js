013a1167f5ac1ceb9244d73cc0436cd7e1a4bc07
Tu Vo <tuvo1106@gmail.com>
2020-03-04 21:48:17 -0800

    Add script to import and delete from JSON

---
+++ dev-data/import-dev-date.js
@@ -0,0 +1,58 @@
+const fs = require("fs")
+const mongoose = require("mongoose")
+const colors = require("colors")
+const dotenv = require("dotenv")
+dotenv.config({ path: "./config.env" })
+
+const Monster = require("./../models/monsterModel")
+
+const { MONGO_CONNECTION_STR, MONGO_PSW } = process.env
+const mongoDB = MONGO_CONNECTION_STR.replace("<PASSWORD>", MONGO_PSW)
+
+mongoose
+  .connect(mongoDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("MongoDB connected.".blue)
+  })
+
+// read JSON
+const monsters = JSON.parse(
+  fs.readFileSync(`${__dirname}/bestiary.json`, "utf-8")
+)
+
+// import data into DB
+const importData = async () => {
+  try {
+    await Monster.create(monsters)
+    console.log("Data sucessfully loaded.")
+    process.exit()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// delete all data from DB
+const deleteData = async () => {
+  try {
+    await Monster.deleteMany()
+    console.log("Data sucessfully deleted.")
+    process.exit()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// waits until mongoose is connected before executing import or delete
+mongoose.connection.once("open", () => {
+  const argv = process.argv[2]
+  if (argv === "--import") {
+    importData()
+  } else if (argv === "--delete") {
+    deleteData()
+  }
+})
