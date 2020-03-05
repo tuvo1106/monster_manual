570393d53a1a72a1f0766754436895f62af5fd5d
Tu Vo <tuvo1106@gmail.com>
2020-03-04 19:40:52 -0800

    Add mongoose to server

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config({ path: "./config.env" })
 
 const app = require("./app")
 
-port = process.env.PORT
+const { MONGO_CONNECTION_STR, MONGO_PSW, PORT } = process.env
+const mongoDB = MONGO_CONNECTION_STR.replace("<PASSWORD>", MONGO_PSW)
+
+mongoose
+  .connect(mongoDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("MongoDB connected.".blue)
+  })
+
+const port = PORT
 app.listen(port, () => {
-  console.log(`App running on ${port}`.red.bold)
+  console.log(`App running on port ${port}`.red.bold)
 })
